1c473f35b679b3c9749a0399c8a5d3ee0085516d
Nguyễn Thành Nam <71055428+thanhnam941999@users.noreply.github.com>
2024-04-12 11:09:30 +0700

    Create function_js_thuong_dung.js

---
+++ function_js_thuong_dung.js
@@ -0,0 +1,8 @@
+// Hàm thay thế ký tự trong chuỗi
+// vd: 
+let date = '09-04-1999'
+let result = ''
+result = date.replace(/-/, "/")
+// result =  '09/04-1999'
+result = date.replace(/-/g, "/")
+// result =  '09/04/1999' khi thêm cờ g(global) vào regex sẽ thay đổi toàn bộ ký tự xuất hiện trong chuõi.
